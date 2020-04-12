c868353e65c7f1b2d427986fa1ad1896c078920b
methkal <methkal.khalawi.7e2@itb.cat>
2020-04-13 01:19:48 +0200

    implementing the module pattern in the app using immediatly invoked functions

---
+++ app.js
@@ -0,0 +1,14 @@
+// segementing our code by using means of modules with immediatly invoked functions
+var budgetController = (function () {
+
+})();
+
+
+var UIController = (function () {
+
+})();
+
+
+var controller = (function (budgetCtrl, UICtrl) {
+    
+})(budgetController, UIController);
