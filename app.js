c19adabd802f6396efa8e9cb6edebcb3322e84dc
methkal <methkal.khalawi.7e2@itb.cat>
2020-04-14 00:56:11 +0200

    reading input data from the UI and create DOMStrings UI variables

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 // segementing our code by using means of modules with immediatly invoked functions
 var budgetController = (function () {})();
 
-var UIController = (function () {})();
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
   // the function that will add new item to the app
   var ctrlAddItem = function () {
     // 1. get the field input data
+    var input = UICtrl.getInput();
+
     // 2. add the item to the budget controller
     // 3. add the item to the UI
     // 4. calculate the budget
     // 5. display the budget in the UI
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (e) {
     if (e.keyCode === 13 || e.which === 13) {
